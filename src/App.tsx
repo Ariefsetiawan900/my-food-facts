b794a96ca1e56ff8f38ecc063cae71306fa3af23
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2024-10-08 03:01:57 +0700

    create pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import ModuleName from '@/pages/test';
 
 const App = () => {
   // const [first, setfirst] = React.useState(false)
   return (
     <div>
       <p className='text-red-500'>hell22222o</p>
+      <ModuleName/>
     </div>
   );
 };
