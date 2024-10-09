531dd0a4bfe5168d44cef4ad55730ddae83f0258
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2024-10-09 14:23:03 +0700

    create routing

---
+++ src/routing/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = React.lazy(() => import('@/pages/Home'));
+const Login = React.lazy(() => import('@/pages/Login'));
+const Detail = React.lazy(() => import('@/pages/Detail'));
+
+const Routing = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/detail/:product-name" element={<Detail />} />
+    </Routes>
+  );
+};
+
+export default Routing;
